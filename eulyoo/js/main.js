12368717c9c2178ec09182bbf342968ce67358b4
chaelim090908 <cofla090908@gmail.com>
2024-06-21 16:53:17 +0900

    0621 을유 wip

--- eulyoo/js/main.js
+++ eulyoo/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){ /* document가 ready(로딩)된 이후 */
+
     const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
 
         effect: "fade", /* fade 효과 */
@@ -15,5 +16,17 @@ $(document).ready(function(){ /* document가 ready(로딩)된 이후 */
             prevEl: '.visual .btn_prev',  
         },
 
-    });
+    }); //.visual .swiper
+
+    const book_swiper = new Swiper('.book .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* li의 넓이 비율로 안함 - css에서 준 넓이대로 함 */
+        spaceBetween: 16, /* li와 li사이 - 제일 작은 여백 */
+        breakpoints: {
+            640: {  /* 640px 이상이 되면 적용 */
+                slidesPerView: "auto",
+                spaceBetween: 24, /* 사이 여백 */ 
+            },
+        },
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    }); /* book_swiper */
 })
