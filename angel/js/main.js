ded98ef230b4901f4a981493a526e5e14c36707e
chaelim090908 <cofla090908@gmail.com>
2024-06-28 18:12:43 +0900

    0628 angel gnb wip

--- angel/js/main.js
+++ angel/js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     const visual_swiper = new Swiper('.visual .swiper', {
         effect: "fade",
         autoplay: {
-            delay: 1000,
+            delay: 100000,
             disableOnInteraction: true,
         },
         loop: true,
@@ -12,7 +12,6 @@ $(document).ready(function(){
         },
     
     });
-    
     
 
     /*
@@ -36,5 +35,28 @@ $(document).ready(function(){
         $('.visual .btn_wrap button.btn_stop').show()
     })
     
+    /*
+        페이지가 스크롤되면 header에 fixed 클래스를 추가
+        다시 맨 위로 올라가면 header에 fixed 클래스를 삭제
+    */
+   let scrolling
+   
+   function scroll_chk(){
+        scrolling = $(window).scrollTop()
+        console.log(scrolling)
+        if(scrolling > 0){
+            $('header').addClass('fixed')
+        }else{
+            $('header').removeClass('fixed')
+        }
+   } //function
+
+   // 문서가 처음 로딩되었을 때 1번 실행
+   scroll_chk()
+
+   $(window).scroll(function(){
+        // 브라우저를 스크롤 할 때마다 실행
+        scroll_chk()
+   })
 
 }) //document
