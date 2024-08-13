eb139a7241120f10fed1dd154c4e6331032aef39
chaelim090908 <cofla090908@gmail.com>
2024-08-13 13:17:43 +0900

    240813 폴바셋 ~ footer wip
    event 다시 해야함

--- paulbassett_0807/js/main.js
+++ paulbassett_0807/js/main.js
@@ -17,7 +17,7 @@ $(document).ready(function(){
 		slidesToShow: 6, //한번에 보일 팝업 수
 		swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
 		centerMode: true, //가운데정렬(가운데가 1번) > 양사이드가 잘림
-		// variableWidth: true, //넓이를 자유롭게 설정
+		variableWidth: true, //넓이를 자유롭게 설정
 		responsive: [
 			{
 			  breakpoint: 1300, //1300px 이하
@@ -29,6 +29,70 @@ $(document).ready(function(){
 			  breakpoint: 1024,
 			  settings: {
 				slidesToShow: 3
+			  }
+			},
+			{
+			  breakpoint: 768,
+			  settings: {
+				slidesToShow: 2
+			  }
+			},
+			{
+			  breakpoint: 375,
+			  settings: {
+				slidesToShow: 1
+			  }
+			},
+		]
+	})
+
+	$('.md .list .popup').slick({
+		slidesToShow: 4, //한번에 보일 팝업 수
+		slidesToScroll: 4, //한번 드래그에 움직이는 슬라이드 제한
+		dots: false, //하단 페이지 버튼 (true, false)
+		arrows: false,  //다음, 이전팝업 (true, false)
+		variableWidth: true, //넓이를 자유롭게 설정
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3
+			  }
+			},
+			{
+			  breakpoint: 768,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},
+			{
+			  breakpoint: 375,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			},
+		]
+	})
+	$('.event .list .popup').slick({
+		dots: false, //하단 페이지 버튼 (true, false)
+		arrows: false,  //다음, 이전팝업 (true, false)
+		slidesToShow: 6, //한번에 보일 팝업 수
+		swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+		variableWidth: true, //넓이를 자유롭게 설정
+		responsive: [
+			{
+			  breakpoint: 1300, //1300px 이하
+			  settings: {
+				slidesToShow: 6
+			  }
+			},
+			{
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 4
 			  }
 			},
 			{
