cd88629517aafcc5050d4ec16fcf3ce30615d448
chaelim090908 <cofla090908@gmail.com>
2024-09-03 13:17:42 +0900

    240903 rest부터 시작

--- 0902_JWhotel/js/main.js
+++ 0902_JWhotel/js/main.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
-    // .pack .list ul li 마우스 오버 시 on 클래스 추가
+    
+	
+	
+	
+	
+	
+	
+	
+	// .pack .list ul li 마우스 오버 시 on 클래스 추가
     $('.pack .list ul li').on('mouseenter', function(){
 		if($(window).width() > 1024){ /* 브라우저 사이즈 1025부터 적용 */
 			$('.pack .list ul li').removeClass('on')
@@ -12,4 +20,12 @@ $(document).ready(function(){
 		$('.pack .list ul li').removeClass('on')
 		$('.pack .list ul li').removeClass('off')
 	})
+
+	/* .exp .tab ul li button click 시 li에 on클래스 추가
+	   .exp .tab ul li.on .tab_detail ul li a photo img가 바뀜
+	*/
+    $('.exp .tab ul li').on('mouseenter', function(){
+        $('.exp .tab ul li').removeClass('on')
+        $(this).addClass('on')
+    })
 })
