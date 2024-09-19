df9014f8c5c3f7f9b0c7feaf01c9c837bd507c60
chaelim090908 <cofla090908@gmail.com>
2024-09-19 18:00:30 +0900

    240919 main css .ass - 반응형

--- 0912_hanhwa/js/common.js
+++ 0912_hanhwa/js/common.js
@@ -35,6 +35,27 @@ $(document).ready(function(){
             $('header .gnb .gnb_wrap .depth1 > li').removeClass('on')
         }
     })
+    $('header .gnb .gnb_wrap ul.depth1 > li > a').on("click", function(e){
+        if(pc_mobile == 'mobile'){
+            e.preventDefault();
+            $(this).parent().toggleClass('open')
+        }
+    });
+    /*
+        header .gnb .gnb_open
+        메뉴열기를 클릭하면 header에 menu_open 클래스 추가
+
+        header .gnb .gnb_close
+        메뉴닫기를 클릭하면 header에 menu_open 삭제
+    */
+   $('header .gnb .gnb_open').on('click', function(){
+    $('header').addClass('menu_open')
+    $("html, body").css({overflow : "hidden", height : $(window).height()}).bind("scroll touchmove mousewheel", function(e){e.preventDefault();e.stopPropagation();return false;},function(){passive:false});
+    })
+    $('header .gnb .gnb_close').on('click', function(){
+    $('header').removeClass('menu_open')
+    $("html, body").css({overflow : "visible", height : "auto"}).unbind('scroll touchmove mousewheel');
+    })
 
     /*
         footer .family .family_open을 클릭하면
